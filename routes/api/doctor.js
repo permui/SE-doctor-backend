cbd1faad2b39887ef3185add58fcffc2be09eb64
Yiping Wang <85465837+Alice-6161@users.noreply.github.com>
2022-06-01 21:42:01 +0800

    Update doctor.js

--- routes/api/doctor.js
+++ routes/api/doctor.js
@@ -73,7 +73,7 @@ router.get('/info/get', async(req, res, next) => {
         .skip((_page_num - 1) * _page_size)
         .limit(_page_size) //page
         .exec()) || [];
-    }else if(dept_id !== null){
+    }else if(_department !== null){
         _data = (await Doctor.find({
             dept_id: _department
         }).sort({ doctor_id: 1 })
