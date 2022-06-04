ae9556bfe235b7f9b85f320052782c3a7ab342f1
bugsz <360307598@qq.com>
2022-06-04 13:55:36 +0800

    move /doctor/add and delete, change return value of /me

--- routes/api/me.js
+++ routes/api/me.js
@@ -6,29 +6,51 @@ const Admin = require("../../models/admin"),
     Doctor = require("../../models/doctor");
 
 router.get('/', async (req, res, next) => {
-    let fail = { status: 202, msg: "not logged in", data: {} };
+    let fail = { status: 401, msg: "not logged in", data: {
+        isLogin: false,
+    }, errorMessage: "请先登录" };
     let u = req.session.user;
     if (u === undefined || u === null) {
         console.log(fail);
+        res.status(401);
         res.json(fail);
         return;
     }
+    console.log(u)
     let r = {};
     if (u.role == consts.role.admin) {
         let a = await Admin.findOne({ adminis_id: u.id });
         r = {
             status: 100,
             msg: "success",
-            data: a
+            data: {
+                ...a,
+                access: u.role
+            }
         };
     } else if (u.role == consts.role.doctor) {
-        let d = await Doctor.findOne({ doctor_id: id });
+        let d = await Doctor.findOne({ doctor_id: u.id });
         r = {
+            success: true,
             status: 100,
             msg: "success",
-            data: d
+            data: {
+                // ...d
+                role: "admin",
+                name: d.name,
+                doctor_id: d.doctor_id,
+                gender: d.gender,
+                age: d.age,
+                position: d.position,
+                department: d.dept_id,
+                access: u.role,
+                avatar: "https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png"
+            }
         };
-    } else r = fail;
+    } else {
+        res.status(401);
+        r = fail;
+    }
 
     console.log(r);
     res.json(r);
